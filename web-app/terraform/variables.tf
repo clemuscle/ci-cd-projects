7e051a90bb9defbf115004512b72af4ca8c9afca
clemuscle <107462603+clemuscle@users.noreply.github.com>
2024-08-15 17:05:02 +0200

    mis en place de variable pour le provider

--- web-app/terraform/variables.tf
+++ web-app/terraform/variables.tf
@@ -1,1 +1,12 @@
 # Placeholder for variable definitions, adjust as needed
+variable "region" {
+  default = "eu-west-3"
+}
+
+variable "aws_access_key" {
+  default = "XXX"
+}
+
+variable "aws_secret_key" {
+  default = "XXX"
+}
